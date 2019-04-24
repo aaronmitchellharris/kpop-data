58bc08380fc0062eb49e53fb4fabf2b238729528
Aaron Harris <aaronmitchellharris@gmail.com>
2019-04-23 23:31:07 -0700

    added picupdate

---
+++ stats/management/commands/picupdate.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand, CommandError
+from stats.models import Group, Video
+
+class Command(BaseCommand):
+    help = 'Updates pictures'
+
+#    def add_arguments(self, parser):
+#        parser.add_argument('poll_id', nargs='+', type=int)
+
+    def handle(self, *args, **options):
+        for g in Group.objects.all():
+            vid = Video.objects.filter(group=g).order_by('upload_date').reverse()[0]
+            vid.updatePic()
+            g.pic_url = vid.thumbnail_url
+            g.save()
